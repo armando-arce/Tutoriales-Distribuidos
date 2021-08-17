862bc06b691e970bef5917c74ec4b125a9e57618
armando-arce <71146244+armando-arce@users.noreply.github.com>
2021-08-16 18:05:15 -0600

    Update index.js

--- node-http-server/src/index.js
+++ node-http-server/src/index.js
@@ -1,9 +1,31 @@
-var http = require("http");
+#!/usr/bin/env node
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+// npm install fastify@3.2
+const server = require('fastify')();
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 4000;
+console.log(`worker pid=${process.pid}`);
+
+server.get('/recipes/:id', async (req, reply) => {
+  console.log(`worker request pid=${process.pid}`);
+  const id = Number(req.params.id);
+  if (id !== 42) {
+    reply.statusCode = 404;
+    return { error: 'not_found' };
+  }
+  return {
+    producer_pid: process.pid,
+    recipe: {
+      id, name: "Chicken Tikka Masala",
+      steps: "Throw it in a pot...",
+      ingredients: [
+        { id: 1, name: "Chicken", quantity: "1 lb", },”
+        “ { id: 2, name: "Sauce", quantity: "2 cups", }
+      ]
+    }
+  };
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Producer running at http://${HOST}:${PORT}`);
+});”
